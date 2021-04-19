099494f14716696f1e89c6f062927b35bbf86ee7
xilo228 <ldzkeymylp@gmail.com>
2021-04-19 16:48:01 +0300

    stuff

---
+++ src/main/java/Cube.java
@@ -0,0 +1,126 @@
+import glm_.mat4x3.Mat4x3;
+import glm_.mat4x4.Mat4;
+import glm_.vec3.Vec3;
+import glm_.vec3.Vec3i;
+import static glm_.glm.*;
+
+import java.util.*;
+
+class Cube {
+    float size = 1f;
+
+    private final Vec3i position;
+    HashMap<edges, Float[]> vertices = new HashMap<>();
+
+    public Cube() {
+        this.position = new Vec3i(0,0,0);
+        genVertices();
+    }
+
+    public enum edges{
+        RIGHT,
+        LEFT,
+        TOP,
+        BOTTOM,
+        FRONT,
+        BACK
+    }
+
+    Cube(Vec3i pos){
+        this.position = pos == null ? new Vec3i(0,0,0) : pos;
+        genVertices();
+    }
+
+    private void genVertices() {
+        int X = position.getX();
+        int Y = -position.getY();
+        int Z = position.getZ();
+        this.vertices.put(edges.BACK, new Float[]{//back
+                0.0f + X, 0.0f + Y, 0.0f + Z, 0.0f, 0.0f,
+                size + X, 0.0f + Y, 0.0f + Z, 1.0f, 0.0f,
+                size + X, size + Y, 0.0f + Z, 1.0f, 1.0f,
+                0.0f + X, size + Y, 0.0f + Z, 0.0f, 1.0f});
+        this.vertices.put(edges.FRONT, new Float[]{//front
+                0.0f + X, 0.0f + Y, size + Z, 0.0f, 0.0f,
+                size + X, 0.0f + Y, size + Z, 1.0f, 0.0f,
+                size + X, size + Y, size + Z, 1.0f, 1.0f,
+                0.0f + X, size + Y, size + Z, 0.0f, 1.0f});
+        this.vertices.put(edges.RIGHT,new Float[]{//RIGHT
+                0.0f + X, size + Y, size + Z, 0.0f, 0.0f,
+                0.0f + X, size + Y, 0.0f + Z, 1.0f, 0.0f,
+                0.0f + X, 0.0f + Y, 0.0f + Z, 1.0f, 1.0f,
+                0.0f + X, 0.0f + Y, size + Z, 0.0f, 1.0f});
+        this.vertices.put(edges.LEFT, new Float[]{//left
+                size + X, size + Y, size + Z, 0.0f, 0.0f,
+                size + X, size + Y, 0.0f + Z, 1.0f, 0.0f,
+                size + X, 0.0f + Y, 0.0f + Z, 1.0f, 1.0f,
+                size + X, 0.0f + Y, size + Z, 0.0f, 1.0f});
+        this.vertices.put(edges.TOP, new Float[]{//top
+                0.0f + X, 0.0f + Y, 0.0f + Z, 0.0f, 0.0f,
+                size + X, 0.0f + Y, 0.0f + Z, 1.0f, 0.0f,
+                size + X, 0.0f + Y, size + Z, 1.0f, 1.0f,
+                0.0f + X, 0.0f + Y, size + Z, 0.0f, 1.0f});
+        this.vertices.put(edges.BOTTOM, new Float[]{//bottom
+                0.0f + X, size + Y, 0.0f + Z, 0.0f, 0.0f,
+                size + X, size + Y, 0.0f + Z, 1.0f, 0.0f,
+                size + X, size + Y, size + Z, 1.0f, 1.0f,
+                0.0f + X, size + Y, size + Z, 0.0f, 1.0f});
+
+    }
+
+    void move(Vec3i pos){
+        Vec3 value = new Vec3();
+        value.plus(pos.getX(), pos.getY(),pos.getZ());
+        genVertices();
+    }
+
+
+
+    public void hideEdge(edges edge){
+        int size = this.vertices.size();
+        switch (edge){
+            case BACK -> {
+                if(vertices.containsKey(edges.BACK)) {
+                    this.vertices.remove(edges.BACK);
+                }
+            }
+            case FRONT -> {
+                if(vertices.containsKey(edges.FRONT)) {
+                    this.vertices.remove(edges.FRONT);
+                }
+            }
+            case RIGHT -> {
+                if(vertices.containsKey(edges.RIGHT)) {
+                    this.vertices.remove(edges.RIGHT);
+                }
+            }
+            case LEFT -> {
+                if(vertices.containsKey(edges.LEFT)) {
+                    this.vertices.remove(edges.LEFT);
+                }
+            }
+            case TOP -> {
+                if(vertices.containsKey(edges.TOP)) {
+                    this.vertices.remove(edges.TOP);
+                }
+            }
+            case BOTTOM -> {
+                if(vertices.containsKey(edges.BOTTOM)) {
+                    this.vertices.remove(edges.BOTTOM);
+                }
+            }
+        }
+    }
+
+    public Vec3i getPosition() {
+        return position;
+    }
+
+    public ArrayList<Float> getVertices(){
+        ArrayList<Float> v = new ArrayList<Float>();
+        for(Map.Entry<edges, Float[]> i : vertices.entrySet()){
+            v.addAll(Arrays.asList(i.getValue()));
+        }
+        return v;
+    }
+}
